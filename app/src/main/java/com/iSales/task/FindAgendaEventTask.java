aa45ece0fb49d2b0864420b15f7010121dd9ee6c
JDevs10 <j-ldsxm18@hotmail.com>
2019-06-25 19:00:37 +0200

    Can Get && Delete Events from isales to dolibarr

--- app/src/main/java/com/iSales/task/FindAgendaEventTask.java
+++ app/src/main/java/com/iSales/task/FindAgendaEventTask.java
@@ -1,13 +1,16 @@
 package com.iSales.task;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.iSales.R;
 import com.iSales.database.AppDatabase;
 import com.iSales.database.entry.UserEntry;
 import com.iSales.interfaces.FindAgendaEventsListener;
 import com.iSales.interfaces.FindOrdersListener;
+import com.iSales.pages.calendar.CalendarActivity;
 import com.iSales.remote.ApiUtils;
 import com.iSales.remote.model.AgendaEvents;
 import com.iSales.remote.rest.AgendaEventsREST;
@@ -30,6 +33,7 @@ public class FindAgendaEventTask extends AsyncTask<Void, Void, AgendaEventsREST>
     private long thirdparty_ids;
 
     private UserEntry userEntry;
+    private ProgressDialog mProgressDialog;
 
     public FindAgendaEventTask(Context context, FindAgendaEventsListener taskComplete, String sortfield, String sortorder, long limit, long page) {
         this.task = taskComplete;
@@ -38,9 +42,16 @@ public class FindAgendaEventTask extends AsyncTask<Void, Void, AgendaEventsREST>
         this.limit = limit;
         this.page = page;
         this.context = context;
+        this.mProgressDialog = new ProgressDialog(context);
         AppDatabase mDb = AppDatabase.getInstance(context.getApplicationContext());
         userEntry = mDb.userDao().getUser().get(0);
         Log.e(TAG, "FindAgendaEventTask: ");
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        showProgressDialog(true, "Agenda", "Synchronisation des évènements en cours...");
     }
 
     @Override
@@ -48,14 +59,16 @@ public class FindAgendaEventTask extends AsyncTask<Void, Void, AgendaEventsREST>
         Log.e(TAG, "doInBackground: ");
 
         String sqlfilters = "fk_user_author="+userEntry.getId();
-//        Requete de connexion de l'internaute sur le serveur
+
+        //Requete de connexion de l'internaute sur le serveur
         Call<ArrayList<AgendaEvents>> call = ApiUtils.getISalesService(context).getAllEvents(sqlfilters, sortfield, sortorder, limit, page);
+        Log.e(TAG, " Url= "+call.request().url());
         try {
             Response<ArrayList<AgendaEvents>> response = call.execute();
-            Log.e(TAG, " Response: "+response);
+            Log.e(TAG, " Response: "+response.body());
             if (response.isSuccessful()) {
                 ArrayList<AgendaEvents> agendaEventsArrayList = response.body();
-                Log.e(TAG, "doInBackground: eventArrayList=" + agendaEventsArrayList.size());
+                Log.e(TAG, "doInBackground: page="+page+" eventArrayList=" + agendaEventsArrayList.size());
 
                 return new AgendaEventsREST(agendaEventsArrayList);
             } else {
@@ -91,5 +104,28 @@ public class FindAgendaEventTask extends AsyncTask<Void, Void, AgendaEventsREST>
         Log.e(TAG, "onPostExecute: ");
 //        super.onPostExecute(findProductsREST);
         task.onFindAgendaEventsTaskComplete(mAgendaEventsREST);
+
+        showProgressDialog(false, null, null);
+    }
+
+    /**
+     * Shows the progress UI and hides.
+     */
+    private void showProgressDialog(boolean show, String title, String message) {
+
+        if (show) {
+            if (title != null) mProgressDialog.setTitle(title);
+            if (message != null) mProgressDialog.setMessage(message);
+
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setCanceledOnTouchOutside(false);
+            mProgressDialog.setProgressDrawable(this.context.getResources().getDrawable(R.drawable.circular_progress_view));
+            mProgressDialog.show();
+        } else {
+            if (mProgressDialog != null){
+                mProgressDialog.dismiss();
+            }
+        }
     }
 }
